47efe56fcf25e2faeab65446c3587cbdc187d67a
TheProjectXmoon <haaekim8@gmail.com>
2026-09-19 00:08:38 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { LanguageProvider } from './locales/LanguageContext';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
